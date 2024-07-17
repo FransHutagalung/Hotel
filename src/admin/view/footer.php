df83145dd3b1e492c97c649acda2711dfbd052de
yedija <yedijasurbakti@gmail.com>
2024-07-17 08:03:24 +0700

    commit succes

---
+++ src/admin/view/footer.php
@@ -0,0 +1,5 @@
+	<footer>
+		<h6>Copyright &copy; 2024 | Yedija Surbakti</h6>
+	</footer>
+</body>
+</html>
---
+++ src/user/view/footer.php
@@ -0,0 +1,9 @@
+
+	</center>	
+		
+	</main>
+	<footer>
+		<h6>Copyright &copy; 2024 | Yedija Surbakti</h6>
+	</footer>
+</body>
+</html>
